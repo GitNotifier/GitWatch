f683896476bd448a9b29607f16baa83ee24da63e
Rushat Gabhane <rushatgabhane@gmail.com>
2022-01-17 06:47:28 +0300

    eslint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from "react";
-class App extends React.Component {
-  render() {
+import React from 'react';
+
+function App() {
     return (
-      <div>
-        <h1>Hello World!</h1>
-      </div>
+        <div>
+            <h1>Hello World!</h1>
+        </div>
     );
-  }
 }
 export default App;
--- src/electron.js
+++ src/electron.js
@@ -1,37 +1,42 @@
-const electron = require("electron");
+const electron = require('electron');
+
 const app = electron.app;
 
 const BrowserWindow = electron.BrowserWindow;
 
-const path = require("path");
-const isDev = require("electron-is-dev");
+const path = require('path');
+const isDev = require('electron-is-dev');
 
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 900,
-    height: 680
-  });
+    mainWindow = new BrowserWindow({
+        width: 900,
+        height: 680,
+    });
 
-  mainWindow.loadURL(
-    isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
-  );
-  mainWindow.on("closed", () => (mainWindow = null));
+    mainWindow.loadURL(
+        isDev
+            ? 'http://localhost:3000'
+            : `file://${path.join(__dirname, '../build/index.html')}`,
+    );
+    mainWindow.on('closed', () => (mainWindow = null));
 }
 
-app.on("ready", createWindow);
+app.on('ready', createWindow);
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
+app.on('window-all-closed', () => {
+    if (process.platform === 'darwin') {
+        return;
+    }
+
     app.quit();
-  }
 });
 
-app.on("activate", () => {
-  if (mainWindow === null) {
+app.on('activate', () => {
+    if (mainWindow != null) {
+        return;
+    }
+
     createWindow();
-  }
 });
